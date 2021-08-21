fc3bc78ca3511513f23225762503af9050459b01
Hasyimi Bahrudin <hasyimi@ridebeam.com>
2021-08-21 15:29:58 +0800

    Working demo

--- src/index.js
+++ src/index.js
@@ -18,6 +18,32 @@ document.body.appendChild(app.view);
 const loader = new Loader();
 loader.use(SpritesheetLoader.use);
 
+import * as Y from 'yjs';
+import { WebrtcProvider } from 'y-webrtc';
+
+function ymapKey(x, y) {
+  const idx = y*20 + x;
+  return idx.toString();
+}
+
+function persistLayer(ydoc, layer, ymap) {
+  ydoc.transact(() => {
+    for (let y = 0; y < 20; y++) {
+      for (let x = 0; x < 20; x++) {
+        const value = layer[y][x];
+        ymap.set(ymapKey(x, y), value);
+      }
+    }
+  });
+}
+
+const ydoc = new Y.Doc();
+const provider = new WebrtcProvider('voffice-crdt', ydoc);
+
+provider.on('synced', synced => {
+  console.log('synced!', synced);
+});
+
 let layers = [];
 
 // Background layer
@@ -106,12 +132,64 @@ loader
   .add('background', 'background.json')
   .load(setupBg);
 
+let player = null;
+let others = [];
+
 const bgLayer = layers[0];
 const fgLayer = layers[1];
 const playersLayer = layers[2];
 
 const bgContainer = new Container();
 const fgContainer = new Container();
+
+let playerTexture = null;
+
+const playersYmap = ydoc.get('players', Y.Map);
+if (playersYmap.size == 0) {
+  persistLayer(ydoc, playersLayer, playersYmap);
+}
+
+playersYmap.observe(() => {
+  const otherTiles = [];
+
+  for (const o of others) {
+    fgContainer.removeChild(o);
+  }
+
+  const tileX = player.position.x;
+  const tileY = player.position.y;
+  for (let y = 0; y < 20; y++) {
+    for (let x = 0; x < 20; x++) {
+      playersLayer[y][x] = playersYmap.get(ymapKey(x, y));
+      if (playersLayer[y][x] > 0 && x != tileX && y != tileY) {
+        otherTiles.push([x, y]);
+      }
+    }
+  }
+
+  if (otherTiles.length > others.length) {
+    for (let i = 0; i < otherTiles.length - others.length; i++) {
+      others.push(new Sprite(playerTexture));
+    }
+  }
+
+  let oi = 0;
+  for (const o of otherTiles) {
+    const otherPlayer = others[oi];
+    oi++;
+
+    otherPlayer.position.set(o[0] * 32, o[1] * 32);
+    fgContainer.addChild(otherPlayer);
+  }
+
+  fgContainer.children.sort(function(a, b) {
+    if (a.position.y > b.position.y) return 1;
+    if (a.position.y < b.position.y) return -1;
+    if (a.position.x > b.position.x) return 1;
+    if (a.position.x < b.position.x) return -1;
+    return 0;
+  });
+});
 
 function setupBg(ld, resources) {
   for (let y = 0; y < 20; y++) {
@@ -130,7 +208,32 @@ function setupBg(ld, resources) {
     .load(setupFg);
 }
 
-let player = null;
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+function spawnPlayer(resources) {
+  playerTexture = resources['foreground'].textures['player.png'];
+  player = new Sprite(playerTexture);
+  fgContainer.addChild(player);
+
+  let x = getRandomInt(0, 20);
+  let y = getRandomInt(0, 20);
+  while (playersYmap.get(ymapKey(x, y)) > 0 || fgLayer[y][x] > 0) {
+    x = getRandomInt(0, 20);
+    y = getRandomInt(0, 20);
+  }
+
+  player.position.set(x * 32, y * 32);
+
+  const playerX = player.position.x;
+  const playerY = player.position.y;
+  const tileX = playerX / 32;
+  const tileY = playerY / 32;
+  playersLayer[tileY][tileX] = 1;
+}
 
 function setupFg(ld, resources) {
   for (let y = 0; y < 20; y++) {
@@ -150,14 +253,7 @@ function setupFg(ld, resources) {
     }
   }
 
-  player = new Sprite(resources['foreground'].textures['player.png']);
-  app.stage.addChild(player);
-
-  const playerX = player.position.x;
-  const playerY = player.position.y;
-  const tileX = playerX / 32;
-  const tileY = playerY / 32;
-  playersLayer[tileY][tileX] = 1;
+  spawnPlayer(resources);
 
   app.stage.addChild(fgContainer);
 }
@@ -198,6 +294,11 @@ document.addEventListener('keydown', (event) => {
     player.position.set(playerX, playerY);
     playersLayer[oldTileY][oldTileX] = 0;
     playersLayer[tileY][tileX] = 1;
+
+    ydoc.transact(() => {
+      playersYmap.set(ymapKey(oldTileX, oldTileY), 0);
+      playersYmap.set(ymapKey(tileX, tileY), 1);
+    });
   
     fgContainer.children.sort(function(a, b) {
       if (a.position.y > b.position.y) return 1;
