eb7773876ae9117b71df62a5d09398af8a8ddc82
Hasyimi Bahrudin <hasyimi@ridebeam.com>
2021-08-21 12:33:28 +0800

    Initial push

---
+++ index.js
@@ -0,0 +1,205 @@
+// Create the application helper and add its render target to the page
+let app = new PIXI.Application({ width: 640, height: 640 });
+document.body.appendChild(app.view);
+
+let layers = [];
+
+// Background layer
+layers.push([
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+]);
+
+// Foreground
+layers.push([
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 2, 2, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+]);
+
+// Players
+layers.push([
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+]);
+
+const bgTextures = {
+  0: 'ground.png',
+  1: 'grass.png',
+};
+
+const fgTextures = {
+  1: 'block.png',
+  2: 'cactus.png',
+};
+
+PIXI.Loader.shared.add('background.json').load(setupBg);
+
+const bgLayer = layers[0];
+const fgLayer = layers[1];
+const playersLayer = layers[2];
+
+const bgContainer = new PIXI.Container();
+const fgContainer = new PIXI.Container();
+
+function setupBg() {
+  const sheet = PIXI.Loader.shared.resources['background.json'].spritesheet;
+
+  for (let y = 0; y < 20; y++) {
+    for (let x = 0; x < 20; x++) {
+      const id = bgLayer[y][x];
+      const tile = new PIXI.Sprite(sheet.textures[bgTextures[id]]);
+      tile.position.set(x * 32, y * 32);
+      bgContainer.addChild(tile);
+    }
+  }
+
+  app.stage.addChild(bgContainer);
+
+  PIXI.Loader.shared.add('foreground.json').load(setupFg);
+}
+
+let player = null;
+
+function setupFg() {
+  const sheet = PIXI.Loader.shared.resources['foreground.json'].spritesheet;
+
+  for (let y = 0; y < 20; y++) {
+    for (let x = 0; x < 20; x++) {
+      const id = fgLayer[y][x];
+      if (id > 0) {
+        const texture = sheet.textures[fgTextures[id]];
+        const tile = new PIXI.Sprite(texture);
+
+        if (texture.height > 32) {
+          tile.anchor.set(0, (texture.height-32)/texture.height);
+        }
+
+        tile.position.set(x * 32, y * 32);
+        fgContainer.addChild(tile);
+      }
+    }
+  }
+
+  player = new PIXI.Sprite(sheet.textures['player.png']);
+  app.stage.addChild(player);
+
+  const playerX = player.position.x;
+  const playerY = player.position.y;
+  const tileX = playerX / 32;
+  const tileY = playerY / 32;
+  playersLayer[tileY][tileX] = 1;
+
+  app.stage.addChild(fgContainer);
+}
+
+document.addEventListener('keydown', (event) => {
+  let playerX = player.position.x;
+  let playerY = player.position.y;
+
+  const oldTileX = playerX / 32;
+  const oldTileY = playerY / 32;
+
+  switch (event.key) {
+    case "Down":
+    case "ArrowDown":
+      playerY += 32;
+      break;
+
+    case "Up":
+    case "ArrowUp":
+      playerY -= 32;
+      break;
+
+    case "Left":
+    case "ArrowLeft":
+      playerX -= 32;
+      break;
+
+    case "Right":
+    case "ArrowRight":
+      playerX += 32;
+      break;
+  }
+
+  const tileX = playerX / 32;
+  const tileY = playerY / 32;
+
+  if (fgLayer[tileY][tileX] == 0 && playersLayer[tileY][tileX] == 0) {
+    player.position.set(playerX, playerY);
+    playersLayer[oldTileY][oldTileX] = 0;
+    playersLayer[tileY][tileX] = 1;
+  
+    fgContainer.children.sort(function(a, b) {
+      if (a.position.y > b.position.y) return 1;
+      if (a.position.y < b.position.y) return -1;
+      if (a.position.x > b.position.x) return 1;
+      if (a.position.x < b.position.x) return -1;
+      return 0;
+    });
+  }
+});
+
+// // Create the sprite and add it to the stage
+// let sprite = PIXI.Sprite.from('sample.png');
+// app.stage.addChild(sprite);
+// 
+// // Add a ticker callback to move the sprite back and forth
+// let elapsed = 0.0;
+// app.ticker.add((delta) => {
+//   elapsed += delta;
+//   sprite.x = 100.0 + Math.cos(elapsed/50.0) * 100.0;
+// });
